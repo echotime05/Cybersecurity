2e997c82586c4ab51d8feabea3196c35d6ed7344
hspyx <1420694415@qq.com>
2026-05-22 18:17:49 +0800

    修改

--- src/roles/tgs/main.cpp
+++ src/roles/tgs/main.cpp
@@ -1,7 +1,9 @@
 #include "cyber/shared/role_runtime.hpp"
+#include <iostream>
 
 // TGS 可执行文件入口，委托统一角色运行时解析参数并启动 TGS 服务。
 int main(int argc, char** argv)
 {
+    std::cout <<"pyx";
     return cyber::run_role_main(cyber::RoleKind::tgs_server, argc, argv);
 }
